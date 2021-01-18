34ac8d8031b9023100ad3e5f2d18f45f314419ef
Akmal Sindorov <asakew@ya.ru>
2021-01-18 11:12:41 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+
+/**
+ * Plugin Name:       AutoMoblie Plugin
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handle the basics with this plugin.
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Akmal Sindorov
+ * Author URI:        https://t.me/asakew
+ * License:           GPL v3
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       AutoMoblie
+ * Domain Path:       /languages
+ */
+
+
+
+ ?>
